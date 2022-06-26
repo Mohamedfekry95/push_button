c1e396811e31e2e8221c6600d5cdc1152afbdd06
Mohamedfekry95 <mohamedelsaid1908@gmail.com>
2022-06-26 21:31:39 +0200

    second commit

---
+++ main.c
@@ -0,0 +1,7 @@
+#include "gpio.h"
+#include "pb.h"
+
+void main()
+{
+    
+}
---
+++ ssd/ssd.h
@@ -0,0 +1,34 @@
+#ifndef SSD_H
+#define SSD_H
+
+
+typedef enum
+{
+    SSD_L,
+    SSD_R
+}tSSD;
+
+typedef enum
+{
+    SSD_OFF,
+    SSD_ON
+}tSSD_state;
+
+typedef enum
+{
+    SSD_0,
+    SSD_1,
+    SSD_2,
+    SSD_3,
+    SSD_4,
+    SSD_5,
+    SSD_6,
+    SSD_7,
+    SSD_8,
+    SSD_9,
+    SSD_NULL
+}tSSD_symbol;
+
+
+
+#endif /* SSD_H */
